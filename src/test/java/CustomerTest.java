3c23b7b600b9a3258166046ab049d110e0a0ba81
seena <seena@DESKTOP-GI3FJUI>
2018-08-06 12:40:38 +0530

    hi

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class CustomerTest {
+@Test
+public void customerTest() {
+	System.out.println("execute custmer Test");
+}
+@Test
+public void customerModificationTest() {
+	System.out.println("execute Modificationcustmer Test");
+}
+
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class UserTest {
+	@Test
+	public void customerTest() {
+		System.out.println("execute custmer Test");
+	}
+	public void editcustomerTest() {
+		System.out.println("execute editcustmer Test");
+	}
+}
